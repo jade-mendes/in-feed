76c051ce3b8e95323817317780cc923c9ec31b88
Jade <jdmsa3@gmail.com>
2025-02-21 11:09:06 -0300

    feat: Header

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,13 @@
+import styles from './styles.module.css'
+import logo from '../../assets/in-logo.png'
+
+function Header(){
+    return (
+        <header className={styles.header}>
+            <img src={logo} alt='Logo da IN Júnior'></img>
+            <h1>Feed</h1>
+        </header>
+    )
+}
+
+export default Header
