64097094c38fda42e220b29410bc8aca7505a3fb
Jade <jdmsa3@gmail.com>
2025-02-22 00:35:25 -0300

    fix: organização do feed

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
 import DevProfile from './components/DevProfile'
 import Header from './components/Header'
 import Post from './components/Post'
+import op1 from './assets/op1.jpeg'
+import op2 from './assets/op2.jpeg'
+import './App.css'
 
 function App() {
 
@@ -9,7 +12,10 @@ function App() {
       <Header />
       <main>
         <DevProfile />
-        <Post profilePicturePath='../../assets/op1.jpeg' name='Elizabeth Best' role='Designer'/>
+        <div>
+          <Post profilePicturePath={op1} name='Elizabeth Best' role='Designer'/>
+          <Post profilePicturePath={op2} name='Ron Ritchie' role='Dev Back-end'/>
+        </div>
       </main>
     </>
   )
