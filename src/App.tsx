12422b7e8e0229500ea6d2093479b54d52d73b10
Jade <jdmsa3@gmail.com>
2025-02-21 23:55:57 -0300

    feat: botão de comentar

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import DevProfile from './components/DevProfile'
 import Header from './components/Header'
+import Post from './components/Post'
 
 function App() {
 
@@ -8,6 +9,7 @@ function App() {
       <Header />
       <main>
         <DevProfile />
+        <Post profilePicturePath='../../assets/op1.jpeg' name='Elizabeth Best' role='Designer'/>
       </main>
     </>
   )
---
+++ src/components/Post/index.tsx
@@ -0,0 +1,41 @@
+import Button from '../Button'
+import styles from './styles.module.css'
+
+interface PostProps {
+    profilePicturePath: string
+    name: string
+    role: string
+}
+
+export default function Post({profilePicturePath, name, role} : PostProps){
+    return(
+        <div className={styles.postContainer}>
+
+            <div className={styles.postHeader}>
+                <div className={styles.opInfoContainer}>
+                    <img src={profilePicturePath} className={styles.opProfilePic}></img>
+                    <div className={styles.opInfo}>
+                        <h4>{name}</h4>
+                        <h6>{role}</h6>
+                    </div>
+                </div>
+                <span>Publicado há {}</span>
+            </div>
+
+            <div className={styles.postContent}>
+                <p>
+                Lorem ipsum dolor sit amet. Ex laboriosam dolorem non tempore earum et voluptatem suscipit ut cupiditate nisi est odit voluptates.
+                Nam magni amet ut ipsam molestiae aut facilis minus et quia reiciendis sed excepturi rerum ex consequatur minima! 
+                Ex rerum sunt et incidunt officia et veritatis deserunt. Sit soluta laboriosam et incidunt sequi et eius fugiat est temporibus similique rem illum natus sit unde eveniet. 
+                Non quos omnis ut autem labore nam vero consequatur est porro similique ad adipisci quisquam!
+                </p>
+            </div>
+
+            <div className={styles.feedbackContainer}>
+                <h4>Deixe seu feedback</h4>
+                <input type='text' placeholder='Escreva um comentário...'></input>
+                <Button />
+            </div>
+        </div>
+    )
+}
