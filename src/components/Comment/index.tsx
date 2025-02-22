1090214e8b05d79bca02b372c95da0e0e748a106
Jade <jdmsa3@gmail.com>
2025-02-22 01:44:04 -0300

    feat: comentário

---
+++ src/components/Comment/index.tsx
@@ -0,0 +1,26 @@
+import styles from './styles.module.css'
+import teste from '../../assets/op3.jpeg'
+import LikeButton from '../LikeButton'
+import TrashButton from '../TrashButton'
+
+
+export default function Comment(){
+    return (
+        <div className={styles.commentContainer}>
+            <img src={teste}></img>
+            <div>
+                <div className={styles.commentBody}>
+                    <div className={styles.commentHeader}>
+                        <div className={styles.opInfo}>
+                            <h4>Fulano</h4>
+                            <span>Cerca de </span>
+                        </div>
+                        <TrashButton />
+                    </div>
+                    <p>Est aspernatur quis eos natus dicta et internos</p>
+                </div>
+                <LikeButton />
+            </div>
+        </div>
+    )
+}
