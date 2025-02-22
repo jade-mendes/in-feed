9e0d85f1caa6f080273d36305fe6ca1e890a6508
Jade <jdmsa3@gmail.com>
2025-02-22 17:09:15 -0300

    styles: mudanças de nome

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,7 +1,15 @@
 import styles from './styles.module.css'
 
+function sendComment(){
+    console.log("comentou")
+}
+
 export default function Button(){
     return(
-        <button className={styles.button}>Comentar</button>
+        <button className={styles.button}
+        onSubmit={sendComment}
+        >
+            Comentar
+        </button>
     )
 }
--- src/components/Comment/index.tsx
+++ src/components/Comment/index.tsx
@@ -12,12 +12,12 @@ export default function Comment({profilePicturePath, name, content} : CommentPro
     return (
         <div className={styles.commentContainer}>
             <img src={profilePicturePath}></img>
-            <div className={styles.bodyAndLikesContainer}>
+            <div className={styles.textAndLikesContainer}>
                 <div className={styles.commentBody}>
                     <div className={styles.commentHeader}>
                         <div className={styles.opInfo}>
                             <h4>{name}</h4>
-                            <span>Cerca de </span>
+                            <span>Cerca de 1h</span>
                         </div>
                         <TrashButton />
                     </div>
